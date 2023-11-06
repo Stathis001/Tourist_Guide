a676f24a82f0306550e0090165ce97e7ff155979
Stathis Vasiloudis <vasiloudisstathis01@gmail.com>
2023-11-06 20:41:51 +0200

    open map on screen - completed

--- app/src/main/java/com/example/touristguide/MainActivity.java
+++ app/src/main/java/com/example/touristguide/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.touristguide;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the SupportMapFragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        SupportMapFragment mapFragment = new SupportMapFragment();
+        fragmentTransaction.add(R.id.mapFragment, mapFragment);
+        fragmentTransaction.commit();
+
+        // Set up the Google Map
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                // You can customize the map and add markers, etc., here.
+            }
+        });
     }
 }
