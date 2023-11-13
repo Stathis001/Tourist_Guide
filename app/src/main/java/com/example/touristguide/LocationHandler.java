7e03bd60a00bd6c4c47f0537cd5df66a21013423
MMitas1ok <117117575+MMitas1ok@users.noreply.github.com>
2023-11-13 19:50:02 +0200

    location handler uptade 2

--- app/src/main/java/com/example/touristguide/LocationHandler.java
+++ app/src/main/java/com/example/touristguide/LocationHandler.java
@@ -12,7 +12,36 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.maps.GoogleMap;
 
-=======
+
 >>>>>>> origin/Marios
 public class LocationHandler {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    private  FragmentActivity activity;
+    private  GoogleMap googleMap;
+    private  FusedLocationProviderClient fusedLocationClient;
+
+    public LocationHandler(FragmentActivity activity, GoogleMap googleMap, FusedLocationProviderClient fusedLocationClient) {
+        this.activity = activity;
+        this.googleMap = googleMap;
+        this.fusedLocationClient = fusedLocationClient;
+    }
+
+    public void initializeLocation() {
+        // Initialize location services here
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            enableMyLocation();
+        } else {
+            // Request location permission
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void enableMyLocation() {
+        // Enable location functionality here
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            googleMap.setMyLocationEnabled(true);
+            startLocationUpdates();
+        }
+    }
 }
