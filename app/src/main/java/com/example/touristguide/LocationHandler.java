dbe8c6c07d6a1aeeac0148aba1374bd1a39aff8a
MMitas1ok <117117575+MMitas1ok@users.noreply.github.com>
2023-11-13 19:22:56 +0200

    Location handler class

---
+++ app/src/main/java/com/example/touristguide/LocationHandler.java
@@ -0,0 +1,15 @@
+package com.example.touristguide;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.maps.GoogleMap;
+
+public class LocationHandler {
+}
