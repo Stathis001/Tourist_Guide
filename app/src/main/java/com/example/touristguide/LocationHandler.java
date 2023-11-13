75dab6785945e00be8cbc69fb8aecae7ff46e5da
kalliotriant <kalliotriant@gmail.com>
2023-11-13 20:03:26 +0200

    LocationHandler update

--- app/src/main/java/com/example/touristguide/LocationHandler.java
+++ app/src/main/java/com/example/touristguide/LocationHandler.java
@@ -44,4 +44,40 @@ public class LocationHandler {
             startLocationUpdates();
         }
     }
+    private void startLocationUpdates() {
+        // Start location updates here
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(5000); // Update every 5 seconds
+        locationRequest.setFastestInterval(3000); // Fastest update interval
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        fusedLocationClient.requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                if (locationResult.getLastLocation() != null) {
+                    Location location = locationResult.getLastLocation();
+                    // You can use the 'location' object to get the latitude and longitude
+                }
+            }
+        }, null);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // Handle permission results here
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableMyLocation();
+            } else {
+                // Handle permission denied
+                //The map works with the sites and every thing but can't provide directions and gps location of user.
+                //This is a whole new class that needs to set these things up.
+            }
+        }
+    }
 }
