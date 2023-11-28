9e321f28122e7d9eff9a153fa3b325b668e432da
Stathis Vasiloudis <vasiloudisstathis01@gmail.com>
2023-11-28 21:32:02 +0200

    Creation of DirectionsHandler.java and modification of other classes in order to work with DirectionsHandler.java

--- app/src/main/java/com/example/touristguide/LocationHandler.java
+++ app/src/main/java/com/example/touristguide/LocationHandler.java
@@ -11,12 +11,15 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
 
 public class LocationHandler extends FragmentActivity {
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-
     private  FragmentActivity activity;
+    private Location location;
+    private LatLng currentUserLocation;
     private  GoogleMap googleMap;
     private  FusedLocationProviderClient fusedLocationClient;
 
@@ -44,26 +47,29 @@ public class LocationHandler extends FragmentActivity {
         }
     }
 
-    private void startLocationUpdates() {
+    public Location startLocationUpdates() {
         // Start location updates here
         LocationRequest locationRequest = new LocationRequest();
         locationRequest.setInterval(5000); // Update every 5 seconds
         locationRequest.setFastestInterval(3000); // Fastest update interval
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            return;
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Handle the case where permissions are not granted
         }
+
         fusedLocationClient.requestLocationUpdates(locationRequest, new LocationCallback() {
             @Override
             public void onLocationResult(LocationResult locationResult) {
                 super.onLocationResult(locationResult);
                 if (locationResult.getLastLocation() != null) {
-                    Location location = locationResult.getLastLocation();
+                    location = locationResult.getLastLocation();
+                    currentUserLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 }
             }
         }, null);
+
+        return location;
     }
 
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -76,6 +82,9 @@ public class LocationHandler extends FragmentActivity {
                 startSimpleMapHandlerActivity();
             }
         }
+    }
+    public LatLng getCurrentUserLocation() {
+        return currentUserLocation;
     }
 
     private void startSimpleMapHandlerActivity() {
