85a9792d2d6273222f7d1eeee04dd1939aee5b2e
Roumen-K <148778885+Roumen-K@users.noreply.github.com>
2023-11-28 20:35:55 +0200

    Update MarkerLoader.java

--- app/src/main/java/com/example/touristguide/MarkerLoader.java
+++ app/src/main/java/com/example/touristguide/MarkerLoader.java
@@ -8,7 +8,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MarkerLoader {
     public MarkerLoader(GoogleMap map) {
         MarkerManager manager = new MarkerManager(map);
-        manager.addMarker(1, 41.07529662776701, 23.55353525970813,"dipae");
-        //we need to add all the markers somehow here.HELP!!!!
+        map.addMarker(new MarkerOptions().position(new LatLng(41.092479913425834, 23.548556365564238)).title("Anastasios eskitzis- Reumatologos").snippet("This is marker 1's snippet"));
+        map.addMarker(new MarkerOptions().position(new LatLng(41.09034269429762, 23.55348399478657)).title("Maria Kokarida - Wrila").snippet("This is marker 1's snippet"));
+        map.addMarker(new MarkerOptions().position(new LatLng(41.08731152657037, 23.55017293627774)).title("Orthopaidiko iatreio enilikwn kai paidwn").snippet("This is marker 1's snippet"));
+        map.addMarker(new MarkerOptions().position(new LatLng(41.08731152657037, 23.55017293627774)).title("Orthopaidiko iatreio enilikwn kai paidwn").snippet("This is marker 1's snippet"));
     }
 }
