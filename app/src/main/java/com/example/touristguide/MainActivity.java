02180e44c2f33e74ef8b99220033b4a32a53106c
MMitas1ok <117117575+MMitas1ok@users.noreply.github.com>
2023-12-03 21:23:16 +0200

    κουμπι για φιλτρα

--- app/src/main/java/com/example/touristguide/MainActivity.java
+++ app/src/main/java/com/example/touristguide/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.touristguide;
+
 import androidx.fragment.app.FragmentActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity implements View.OnClickListener {
+public class MainActivity extends FragmentActivity implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
 
-    Button startButton ;
+    Button startButton;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +20,55 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         startButton = findViewById(R.id.startButton);
         startButton.setOnClickListener(this);
     }
+
     @Override
     public void onClick(View v) {
         Intent intent = new Intent(v.getContext(), MapsActivity.class);
         startActivity(intent);
+    }
+
+    public void showPopup(View filter) {
+        PopupMenu popup = new PopupMenu(this, filter);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.popup_filters_menu);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+       /* switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item4:
+                Toast.makeText(this, "Item 4 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }*/
+        int id = item.getItemId();
+        if (id==R.id.item1){
+            Toast.makeText(this, "Item 1 clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id==R.id.item2) {
+            Toast.makeText(this, "Item 2 clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id==R.id.item3) {
+            Toast.makeText(this, "Item 3 clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id==R.id.item4) {
+            Toast.makeText(this, "Item 4 clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
