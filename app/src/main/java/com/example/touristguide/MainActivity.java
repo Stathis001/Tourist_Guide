4ac10185e77b7f8f7a5bd1aad279befa67f2ed67
Argiris Amanatidis <135833960+Argiris2000@users.noreply.github.com>
2023-12-04 16:21:55 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/touristguide/MainActivity.java
+++ app/src/main/java/com/example/touristguide/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.res.Configuration;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Locale;
+
+
 
 public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
@@ -21,4 +26,35 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         startActivity(intent);
     }
 
+}
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_screen);
+
+        Button languageButton = findViewById(R.id.languageButton);
+        languageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Καλέστε τη μέθοδο για την αλλαγή της γλώσσας
+                changeLanguage("en"); // Εδώ χρησιμοποιείται η κωδική ονομασία για τα Αγγλικά ("en")
+            }
+        });
+    }
+
+    // Μέθοδος για την αλλαγή της γλώσσας
+    private void changeLanguage(String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+
+        Configuration configuration = new Configuration();
+        configuration.setLocale(locale);
+
+        getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+
+        // Εδώ μπορείτε να ξανα-φορτώσετε τη δραστηριότητά σας για να εφαρμοστεί η αλλαγή
+        recreate();
+    }
 }
